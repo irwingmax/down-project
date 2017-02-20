3ce44463f6efb875e235a138314b6f0bea6499cd
irwing-max <irwing.1911@gmail.com>
2017-02-20 14:25:17 -0300

    news

--- index.php
+++ index.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="e">
 <head>
 	<meta charset="UTF-8">
 	<title>A Lenda dos Carecas</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
+	<div class="container">
+		<div class="col-sm-12 header">
+			
 	
+		</div>
+
+	</div>
 </body>
 </html>
