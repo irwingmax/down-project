942b85705fcfce960c942b8c262467890d2a2769
irwing19 <irwing.1911@gmail.com>
2017-05-22 16:42:11 -0300

    update

--- game.php
+++ game.php
@@ -5,7 +5,6 @@
 				<h2 class="titulo-page">O JOGO</h2>
 			</div>
 		</div>
-		
 	</div>
 	<article>
 		<div class="row">
@@ -13,7 +12,6 @@
 				<p class="ajuste-texto">
 					O projeto tem a finalidade de empregar melhorias aos procedimentos de tratamento psicológicos de crianças com câncer utilizando aplicações tecnológicas.
 					Atualmente um dos principais impasses é a questão suporte psicológico a crianças que estão passando pelo tratamento de câncer. Cada centro de tratamento deve possuir uma área específica para as crianças e que durante o período haja interação entre elas já que estão passando pelo mesmo tipo de situação porém isto nem sempre acontece.
-
 				</p>
 			</div>
 			<div class="col-sm-4">
@@ -27,11 +25,8 @@
 			<div class="col-sm-8">
 				<p class="ajuste-texto">
 					Devido a influência da tecnologia e aos benefícios advindos dos jogos eletrônicos como terapia, é possível concluir que o aplicativo auxiliará de maneira eficaz no desempenho do paciente, uma vez que a utilização deste método ajudará na auto-estima da criança. Além disso, a plataforma facilitará a compreensão de sua doença e ajudará a passar por seu tratamento de maneira lúdica e didática.
-
-					
 				</p>
 			</div>
 		</div>
 	</article>
-	
 <?php include("footer.php"); ?>
